a4b793d2f9cafc82d4544f45442a6969aa2a0524
maria <102561475+arlebiina@users.noreply.github.com>
2026-07-08 17:01:53 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
-// Inicializa o roteamento simples do padrão MVC
-require_once __DIR__ . '/../src/models/Projeto.php';
-require_once __DIR__ . '/../src/controllers/PortfolioController.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Raiz do servidor htdocs
+$raizSrv = '/home/vol5_7/infinityfree.com/if0_42351799/maria-portfcode.talk4fun.net/htdocs';
+
+// CORREÇÃO: Incluindo as subpastas corretas que o diagnóstico encontrou
+require_once $raizSrv . '/src/models/Projeto.php';
+require_once $raizSrv . '/src/controllers/PortfolioController.php';
 
 $controller = new PortfolioController();
 
-// Captura a ação da URL. Se não houver, a padrão é a home
+// Captura a ação da URL
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
 if ($action === 'contato') {
@@ -14,4 +21,3 @@ if ($action === 'contato') {
     $controller->index();
 }
 ?>
-
